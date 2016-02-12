b0a004512f56c53ebb16962f79f383c85acbe67e
hrscy <15399070815@163.com>
2016-02-12 10:21:33 +0800

    新建项目

---
+++ ImitateBaisi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImitateBaisi
+//
+//  Created by 杨蒙 on 16/2/12.
+//  Copyright © 2016年 hrscy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImitateBaisi/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImitateBaisi
+//
+//  Created by 杨蒙 on 16/2/12.
+//  Copyright © 2016年 hrscy. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
