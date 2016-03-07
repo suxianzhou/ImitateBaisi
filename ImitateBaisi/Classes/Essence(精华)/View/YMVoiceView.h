d1ea4b960dcd4e0684fdbcd94487188e49ff8c86
hrscy <15399070815@163.com>
2016-03-07 23:19:34 +0800

    创建声音的view

---
+++ ImitateBaisi/Classes/Essence(精华)/View/YMVoiceView.h
@@ -0,0 +1,13 @@
+//
+//  YMVoiceView.h
+//  ImitateBaisi
+//
+//  Created by 杨蒙 on 16/3/7.
+//  Copyright © 2016年 hrscy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YMVoiceView : UIView
+
+@end
---
+++ ImitateBaisi/Classes/Essence(精华)/View/YMVoiceView.m
@@ -0,0 +1,21 @@
+//
+//  YMVoiceView.m
+//  ImitateBaisi
+//
+//  Created by 杨蒙 on 16/3/7.
+//  Copyright © 2016年 hrscy. All rights reserved.
+//
+
+#import "YMVoiceView.h"
+
+@implementation YMVoiceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
